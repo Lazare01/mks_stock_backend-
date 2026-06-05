30756e261789621a3aabf19f6c5b0cfc8452bca5
Lazare <lazaredeveloppeur.com>
2026-06-05 06:47:04 +0200

    add user branch

--- core/views.py
+++ core/views.py
@@ -1,27 +1,32 @@
 # =========================================================
 # core/views.py
 # =========================================================
-
+from django.utils import timezone
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from django.shortcuts import get_object_or_404
+
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from rest_framework.decorators import action, permission_classes, api_view
 
-from .models import User
+from .models import User,UserWarehouseAccess
 from .serializers import (
     RegisterUserSerializer,
     ObtenTokenPairSerializer,
     UsersListSerialiser,
     UserCreateSerializer,
 )
+from core.constants.user_roles import UserRole
 
 from .base_viewset import SoftDeleteModelViewSet
 
 from .permissions import CanUserManager
+
+from inventory.models  import Warehouse,WarehouseType,ManagerAssignment
 
 
 class AuthViewSet(viewsets.GenericViewSet):
@@ -115,6 +120,10 @@ class UsersViewSet(SoftDeleteModelViewSet):
 
         return User.objects.exclude(is_superuser=True).exclude(id=self.request.user.id)
 
+    # =======================================
+    # ============= delete user =============
+    # =======================================
+
     @action(detail=True, methods=["delete"], url_path="hard-delete")
     def hard_delete(self, request, pk=None):
 
@@ -125,6 +134,104 @@ class UsersViewSet(SoftDeleteModelViewSet):
         return Response(
             {"message": "Utilisateur supprimé définitivement."},
             status=status.HTTP_204_NO_CONTENT,
+        )
+
+    # =======================================
+    # ============= affecter user  =============
+    # =======================================
+
+    @action(
+        detail=True,
+        methods=["post"],
+        url_path="assign-branch",
+    )
+    def assign_branch(self, request, pk=None):
+
+        manager = self.get_object()
+
+        warehouse_id = request.data.get("warehouse_id")
+
+        if not warehouse_id:
+            return Response(
+                {"detail": "warehouse_id est obligatoire."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if manager.role != UserRole.BRANCH_MGR:
+            return Response(
+                {
+                    "detail": (
+                        "Seuls les utilisateurs ayant le rôle "
+                        "BRANCH_MGR peuvent être affectés."
+                    )
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        warehouse = get_object_or_404(
+            Warehouse,
+            id=warehouse_id,
+        )
+
+        if warehouse.warehouse_type != WarehouseType.BRANCH:
+            return Response(
+                {"detail": ("Le manager doit être affecté " "à une succursale.")},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        # ==========================================
+        # Désactivation des anciens accès
+        # ==========================================
+
+        UserWarehouseAccess.objects.filter(
+            user=manager,
+            is_active=True,
+        ).update(
+            is_active=False,
+        )
+
+        # ==========================================
+        # Création du nouvel accès
+        # ==========================================
+
+        UserWarehouseAccess.objects.update_or_create(
+            user=manager,
+            warehouse=warehouse,
+            defaults={
+                "can_view": True,
+                "can_manage_stock": True,
+                "can_transfer_stock": True,
+                "can_manage_sales": True,
+                "can_manage_installations": True,
+                "is_active": True,
+            },
+        )
+
+        # ==========================================
+        # Affectation du manager
+        # ==========================================
+
+        assignment = ManagerAssignment.objects.create(
+            manager=manager,
+            warehouse=warehouse,
+            start_date=timezone.now().date(),
+            is_active=True,
+        )
+
+        return Response(
+            {
+                "message": "Manager affecté avec succès.",
+                "assignment_id": assignment.id,
+                "manager": {
+                    "id": manager.id,
+                    "email": manager.email,
+                },
+                "warehouse": {
+                    "id": warehouse.id,
+                    "name": warehouse.name,
+                },
+            },
+            status=status.HTTP_201_CREATED,
         )
 
 
--- inventory/views.py
+++ inventory/views.py
@@ -1,12 +1,22 @@
 from django.db.models import Count, Q
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
 
-from rest_framework import viewsets
+from rest_framework import viewsets, views, status
+from rest_framework.response import Response
+
 from rest_framework.permissions import IsAuthenticated
 
-from inventory.models import Warehouse,StockItemStatus
+from inventory.models import Warehouse, StockItemStatus
 from inventory.serializers import WarehouseSerializer
 
 from core.services.access_services import AccessService
+from core.permissions import CanUserManager
+
+from core.models import UserWarehouseAccess,User
+from core.constants.user_roles import UserRole
+
+from .models import Warehouse, WarehouseType, ManagerAssignment
 
 
 class WarehouseViewSet(viewsets.ModelViewSet):
@@ -24,17 +34,10 @@ class WarehouseViewSet(viewsets.ModelViewSet):
         # =====================================================
 
         queryset = Warehouse.objects.annotate(
-
             available_stock_count=Count(
-                "stock_items",
-                filter=Q(
-                    stock_items__status=StockItemStatus.AVAILABLE
-                )
+                "stock_items", filter=Q(stock_items__status=StockItemStatus.AVAILABLE)
             )
-
-        ).prefetch_related(
-            "manager_assignments__manager"
-        )
+        ).prefetch_related("manager_assignments__manager")
 
         # =====================================================
         # SUPER ADMIN
@@ -42,27 +45,92 @@ class WarehouseViewSet(viewsets.ModelViewSet):
 
         if user.is_superuser:
             return queryset
-        
+
         # =====================================================
         # SUPER ADMIN
         # =====================================================
-        
+
         if user.role == "CENTRAL_MGR":
             return queryset
-        
 
         # =====================================================
         # IDS DES AGENCES AUTORISEES
         # =====================================================
 
-        warehouse_ids = AccessService.get_user_warehouse_ids(
-            user
-        )
+        warehouse_ids = AccessService.get_user_warehouse_ids(user)
 
         # =====================================================
         # FILTRAGE SECURISE
         # =====================================================
 
-        return queryset.filter(
-            id__in=warehouse_ids
+        return queryset.filter(id__in=warehouse_ids)
+
+
+# =============================================
+# =================== Assigment views
+# =============================================
+
+
+class AssignBranchManagerView(views.APIView):
+
+    permission_classes = [CanUserManager]
+
+    def post(self, request, user_id):
+
+        warehouse_id = request.data.get("warehouse_id")
+
+        manager = get_object_or_404(User, id=user_id)
+
+        warehouse = get_object_or_404(Warehouse, id=warehouse_id)
+
+        if manager.role != UserRole.BRANCH_MGR:
+            return Response(
+                {
+                    "detail": (
+                        "Cet utilisateur n'est pas un " "gestionnaire de succursale."
+                    )
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if warehouse.warehouse_type != WarehouseType.BRANCH:
+            return Response(
+                {"detail": ("Le manager doit être affecté " "à une succursale.")},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        # ==========================================
+        # Accès à la succursale
+        # ==========================================
+
+        UserWarehouseAccess.objects.update_or_create(
+            user=manager,
+            warehouse=warehouse,
+            defaults={
+                "can_view": True,
+                "can_manage_stock": True,
+                "can_transfer_stock": True,
+                "can_manage_sales": True,
+                "can_manage_installations": True,
+                "is_active": True,
+            },
+        )
+
+        # ==========================================
+        # Affectation du manager
+        # ==========================================
+
+        assignment = ManagerAssignment.objects.create(
+            manager=manager,
+            warehouse=warehouse,
+            start_date=timezone.now().date(),
+            is_active=True,
+        )
+
+        return Response(
+            {
+                "message": "Manager affecté avec succès.",
+                "assignment_id": assignment.id,
+            },
+            status=status.HTTP_201_CREATED,
         )
